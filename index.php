0e38aa1a66eb884dc8d40c393778e4176582646f
jeremy <jeremy.nowak@laplateforme.io>
2023-05-02 17:10:07 +0200

    test

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+require 'vendor/autoload.php';
+$router = new AltoRouter();
+$router->setBasePath('/super-week');
+
+$router->map('GET', '/', function () {
+    echo "<h1>Bienvenu sur l’accueil</h1>";
+}, 'home');
+
+$router->map('GET', '/users', function () {
+    echo "<h1>Bienvenu sur la liste des Utilisateurs.</h1>";
+}, 'users');
+
+$router->map('GET', '/users/[i:id]', function ($id) {
+    echo "<h1>Bienvenue sur la page de l'utilisateur $id</h1>";
+}, 'user');     
+
+
+
+
+// match
+$match = $router->match();
+
+// call closure or throw 404 status
+if (is_array($match) && is_callable($match['target'])) {
+    call_user_func_array($match['target'], $match['params']);
+} else {
+    // no route was matched
+    header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
+
+?>
+
+<?php
+var_dump($_SERVER)
+?>
