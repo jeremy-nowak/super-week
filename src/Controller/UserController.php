be0ed78164c872ce38759b49626869d37c102217
jeremy <jeremy.nowak@laplateforme.io>
2023-05-03 13:26:45 +0200

    mise en place du MVC et remplissage BDD

---
+++ src/Controller/UserController.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Controller;
+
+use Faker;
+use App\Model\UserModel;
+
+class UserController{
+
+    public function fill(){
+        
+        $faker = Faker\Factory::create();
+
+        for ($i = 0; $i < 20; $i++) {
+            $lastname = $faker->lastName();
+            $firstname = $faker->firstName();
+            $email = strtolower("$firstname.$lastname@$faker->freeEmailDomain");
+            $userModel = new UserModel();
+            $userModel->fillBdd([
+                "firstname"=>$firstname,
+                "lastname"=>$lastname,
+                "email"=>$email,
+                "password"=>password_hash("azerty", PASSWORD_DEFAULT),
+            ]);
+            
+
+        }
+    }
+}
+
+?>
